3f13c18fc896e7d0545c201f62b5ab6095afd364
Martosky <imoniaromartins@yahoo.com>
2025-02-21 15:11:39 +0100

    clean space

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+
+export default function Header(){
+    return (
+        <>
+          <header>
+            <nav>
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+                <a href="#article">Article</a>
+            </nav>
+          </header>
+        </>
+    )
+}
