498238b3119de87af6ebc0180214d7b50af7c968
Martosky <imoniaromartins@yahoo.com>
2025-03-06 11:06:38 +0100

    more done

--- src/Secondpage.jsx
+++ src/Secondpage.jsx
@@ -1,13 +1,5 @@
 export default function Secondpage(props){
     return (
-        <form>
-          <select>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-          </select>
-        </form>
+        
     )
 }
